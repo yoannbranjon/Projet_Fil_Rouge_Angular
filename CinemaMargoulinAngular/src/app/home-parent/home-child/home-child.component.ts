c9f373727bdbecf57ad2ca7f5216c3c1cb99a36e
DESKTOP-5L7N2A8\ib <thualanthony@gmail.com>
2021-06-02 13:36:48 +0200

    angular material + themes terminés!

---
+++ CinemaMargoulinAngular/src/app/home-parent/home-child/home-child.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-child',
+  templateUrl: './home-child.component.html',
+  styleUrls: ['./home-child.component.scss']
+})
+export class HomeChildComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

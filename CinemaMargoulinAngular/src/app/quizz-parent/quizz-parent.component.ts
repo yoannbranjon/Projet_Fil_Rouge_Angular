04caad50f5c993f41e65f9d5fc1e71a834af27d1
DESKTOP-5L7N2A8\ib <bouhafsnezha@hotmail.fr>
2021-06-05 10:29:12 +0200

    je sais pas quel modif sur le quiz il marche pas de toute maniére

--- CinemaMargoulinAngular/src/app/quizz-parent/quizz-parent.component.ts
+++ CinemaMargoulinAngular/src/app/quizz-parent/quizz-parent.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizzParentComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    
+   }
 
   ngOnInit(): void {
   }
